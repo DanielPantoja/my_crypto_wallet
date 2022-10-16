1bed6da161256d6cafda3d6b9a9e4a6c1f592047
Daniel Pantoja <pantojad84@yahoo.com>
2022-10-16 09:26:59 -0500

    added controller and routes

--- server/backend/app.js
+++ server/backend/app.js
@@ -5,7 +5,7 @@ const path = require("path");
 app.use(express.json());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-// require('./routes')(app);
+require('./routes')(app);
 app.use('/', express.static( path.join(__dirname, '../../public/dist/public') ));
 app.use((req, res, next) => {
     res.sendFile(path.join(__dirname, '../../public/dist/public/index.html'));
