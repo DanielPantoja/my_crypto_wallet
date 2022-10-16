fea5bad7bac70d455339ec5797a148118e252c34
Daniel Pantoja <pantojad84@yahoo.com>
2022-10-16 08:54:39 -0500

    added add coin api

--- server/backend/routes.js
+++ server/backend/routes.js
@@ -4,5 +4,6 @@ module.exports = (app) => {
     app.get('/api/test', (req, res) => data.testApi(req, res));
     app.get('/api/test/2', (req, res) => data.secondApiTest(req, res));
     app.post('/api/login', (req, res) => data.login(req, res));
-    app.post('/api/signup', (req, res) => data.signUp(req, res))
+    app.post('/api/signup', (req, res) => data.signUp(req, res));
+    app.post('/api/addCoin', (req,res) => data.addCoin(req, res));
 }
