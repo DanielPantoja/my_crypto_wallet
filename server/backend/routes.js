ab93bcb1abef0e648d01fca9808c8526867240e6
Daniel Pantoja <pantojad84@yahoo.com>
2022-10-16 09:27:01 -0500

    added login & signup api

--- server/backend/routes.js
+++ server/backend/routes.js
@@ -3,4 +3,6 @@ const data = require('../controllers/data');
 module.exports = (app) => {
     app.get('/api/test', (req, res) => data.testApi(req, res));
     app.get('/api/test/2', (req, res) => data.secondApiTest(req, res));
+    app.post('/api/login', (req, res) => data.login(req, res));
+    app.post('/api/signup', (req, res) => data.signUp(req, res))
 }
--- server/controllers/data.js
+++ server/controllers/data.js
@@ -20,5 +20,74 @@ module.exports = {
             return res.json({ response: data['data']})
             // return res.json({ response: data['message'] })
         })
-    }
+    },
+    login: (req, res) => {
+        let fetchedUser;
+        User.findOne({ email: req.body.email })
+            .then(user => {
+                if (!user) {
+                    return
+                }
+                fetchedUser = user;
+                return bcrypt.compare(req.body.password, user.password);
+            })
+            .then(result => {
+                if (!result) {
+                    console.log('something went wrong here')
+                    return res.status(404).json({ message: 'Invalid Credentialss' })
+                }
+                const token = jwt.sign(
+                    { email: fetchedUser.email, userId: fetchedUser._id },
+                    process.env.JWT_KEY,
+                    { expiresIn: "1h" }
+                );
+                console.log(token)
+                return res.status(200).json({ token: token, expiresIn: 3600, userId: fetchedUser._id });
+            })
+            .catch(err => {
+                //at this part max would change the authstatuslistner
+                console.log(err)
+                return res.status(404).json({ message: 'Invalid Credentials', error: err })
+            });
+    },
+    signUp: (req, res) => {            User.find({ email: req.body.email })
+                .exec()
+                .then(user => {
+                    if (user.length >= 1) {//need to test this to make sure it doesnt crash app
+                        return res.status(404).json({ message: "User with this email exists" })//User with this email exists 
+                    }
+                    if (req.body.password.length <= 7) {
+                        return res.status(404).json({ message: "Password must be atleast 8 charcters" })
+                    }
+                    else {
+                        bcrypt.hash(req.body.password, 10, (err, hash) => {
+                            if (err) {
+                                return res.status(404).json({ message: "Error creating hashed password for User" })
+                            }
+                            else {
+                                const user = new User(
+                                    {
+                                        _id: new mongoose.Types.ObjectId(),
+                                        first_name: req.body.first_name,
+                                        last_name: req.body.last_name,
+                                        email: req.body.email,
+                                        password: hash
+                                    });
+                                user.save()
+                                    .then(() => {//test to see if this works or do i need to use a variable like result 
+                                        const token = jwt.sign({ email: user.email, userId: user._id }, process.env.JWT_KEY, { expiresIn: "1h" });
+                                        return res.status(200).json({ token: token, expiresIn: 3600, userId: user._id })
+                                    })
+                                    .catch(err => {
+                                        return res.status(404).json({ message: 'Internal database error, sorry', error: err.errors })
+                                    }
+                                )
+                            }
+                        })
+                    }
+                })
+                .catch(err => {
+                    return res.status(404).json({ message: 'Internal database error, cannot find User', error: err.errors })
+                })
+            }
 }
