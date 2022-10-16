31f36331acbe9de4e5155ab358817adb902c062a
Daniel Pantoja <pantojad84@yahoo.com>
2022-10-16 09:26:59 -0500

    installed coingecko api

--- server/controllers/data.js
+++ server/controllers/data.js
@@ -1,5 +1,21 @@
+const CoinGeckoClient = require('../backend/app');
+
 module.exports = {
+    // testApi: (req, res) => {
+    //     return res.json({ message: 'this is the response feedback' })
+    // },
+
     testApi: (req, res) => {
-        return res.json({ message: 'this is the response feedback' })
+        // let data = await CoinGeckoClient.ping();
+        // return res.json({message: data})
+
+        // await CoinGeckoClient.ping().then(data => {
+        //     return res.json({message: data})
+        // })
+        CoinGeckoClient.ping().then(data => {
+            return res.json({res: data})
+        })
+        // let data = CoinGeckoClient.ping()
+        // return res.json({res: data})
     }
 }
